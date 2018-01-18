7ca9933732782f286c7d66fa81eaed40c82b415c
Amit Bendale <amit.bendale@iongroup.com>
2018-01-18 17:33:01 +0530

    First callbacck

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 var rect = require('./rectangle')
 
-function solveRectangle(l,b){
-    console.log("Solving l "+l+" b "+b);
-    if(l <=0 || b<= 0){
-        console.log("Invalid dimension");
-    }
-    else{
-        console.log("area: "+rect.area(l,b));
-        console.log("Perimeter: "+rect.perimeter(l,b))
-    }
-}
+function solveRectangle(l,b) {
+    console.log("Solving for rectangle with l = "
+                + l + " and b = " + b);
+    rect(l,b, (err,rectangle) => {
+        if (err) {
+	        console.log("ERROR: ", err.message);
+	    }
+        else {
+            console.log("The area of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.area());
+            console.log("The perimeter of the rectangle of dimensions l = "
+                + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("This statement after the call to rect()");
+};
+
 
 solveRectangle(2,4);
 solveRectangle(3,5);
